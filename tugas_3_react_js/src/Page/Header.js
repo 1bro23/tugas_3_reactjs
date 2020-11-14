4684851e90f59fb19115e29c016ad1e663798ed6
1bro23 <shinlia21@gmail.com>
2020-11-14 14:12:50 +0800

    add tugas 3 reactjs

---
+++ tugas_3_react_js/src/Page/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+    return(
+      <div>
+        <b><center>
+          <a>Home </a>
+          <a>| Product </a>
+          <a>| Kontak </a>
+          <a>| Tentang Kami</a>
+        </center></b>
+      </div>
+    );
+}
+export default Header;
